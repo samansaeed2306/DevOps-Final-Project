096432f2008464c62e41694cc7874fe0ab6ae105
openai-exe <sabeensaeed06@gmail.com>
2023-12-07 01:11:51 +0500

    Add Database Doctors

---
+++ doctors/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS 
+from flask_pymongo import PyMongo
+from bson import ObjectId
+app = Flask(__name__)
+CORS(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/DevOps"
+mongo = PyMongo(app)
+# doctors = [
+#   { 'id': "1",'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality':"DevOps"  },
+#   { 'id': "2",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  }
+# ]
+
+@app.route('/hello')
+def hello():
+  greeting = "Hello world!"
+  return greeting
+
+@app.route('/check_mongodb_connection')
+def check_mongodb_connection():
+    try:
+        mongo.db.doctors.find_one()
+        return jsonify({"status": "success", "message": "MongoDB connection successful"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": f"MongoDB connection error: {str(e)}"}), 500
+
+@app.route('/doctors', methods=["GET"])
+def getDoctors():
+  # doctors = list(mongo.db.doctors.find())
+  # return jsonify(doctors)
+  try:
+        doctors = list(mongo.db.doctors.find())
+
+        # Convert ObjectId to string in each document
+        for doctor in doctors:
+            doctor['_id'] = str(doctor['_id'])
+
+        return jsonify(doctors)
+  except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/doctor/<id>', methods=["GET"])
+def getDoctor(id):
+    doctor = mongo.db.doctors.find_one({'id': id})
+    return jsonify(doctor)
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0",port=9090)
---
+++ frontend/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const express = require('express');
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const axios = require('axios');
+
+// App
+const app = express();
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html'); // Adjust the path to your HTML file
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Hello world\n');
+});
+
+// Define your API endpoints and routes here
+app.get('/api/doctors', async (req, res) => {
+  let url  = process.env.DOCTORS_SERVICE_URL;
+  console.log('Doctors Service URL:', process.env.DOCTORS_SERVICE_URL);
+
+  try {
+    const response = await axios.get(String(`http://localhost:9090/doctors`));
+    const doctors = response.data;
+    res.json(doctors);
+  } catch (error) {
+    console.error('Error fetching doctors:', error);
+    res.status(500).json({ error: 'Could not fetch doctors' });
+  }
+});
+
+app.get('/api/appointments', async (req, res) => {
+  let url  = process.env.APPOINTMENTS_SERVICE_URL;
+  console.log('Appointments Service URL:', url);
+
+  try {
+    const response = await axios.get(String(`http://localhost:7070/appointments`));
+    const appointments = response.data;
+    res.json(appointments);
+  } catch (error) {
+    console.error('Error fetching appointments:', error);
+    res.status(500).json({ error: 'Could not fetch appointments' });
+  }
+});
+
+app.listen(PORT, HOST); 
+console.log(`Running on http://${HOST}:${PORT}`);
