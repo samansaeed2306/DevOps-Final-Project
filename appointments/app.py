a3a941ea1c87b0b467dc3c9005259f4def4a083c
openai-exe <sabeensaeed06@gmail.com>
2023-12-07 01:25:59 +0500

    Add Database to Appointments

--- appointments/app.py
+++ appointments/app.py
@@ -1,28 +1,53 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS 
+from flask_pymongo import PyMongo
+from bson import ObjectId
 app = Flask(__name__)
 CORS(app)
-appointments = [
-  { 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Good"  },
-  { 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-  { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-]
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Appointments"
+mongo = PyMongo(app)
+# appointments = [
+#   { 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Good"  },
+#   { 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
+#   { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+#   { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
+#   { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+# ]
 
 @app.route('/hello')
 def hello():
   greeting = "Hello world!"
   return greeting
 
+@app.route('/check_mongodb_connection')
+def check_mongodb_connection():
+    try:
+        mongo.db.appointments.find_one()
+        return jsonify({"status": "success", "message": "MongoDB connection successful"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": f"MongoDB connection error: {str(e)}"}), 500
+    
+    
 @app.route('/appointments', methods=["GET"])
 def getAppointments():
-  return jsonify(appointments)
+  # return jsonify(appointments)
+  try:
+        appointments = list(mongo.db.appointments.find())
+
+        # Convert ObjectId to string in each document
+        for appointment in appointments:
+            appointment['_id'] = str(appointment['_id'])
+
+        return jsonify(appointments)
+  except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/appointment/<id>', methods=["GET"])
 def getAppointment(id):
-  id = int(id) - 1
-  return jsonify(appointments[id])
+  # id = int(id) - 1
+  # return jsonify(appointments[id])
+  appointment = mongo.db.appointments.find_one({'id': id})
+  return jsonify(appointment)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",port=7070)
