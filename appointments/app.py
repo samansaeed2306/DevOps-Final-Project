4de17fff28c5a6ebad4adcad1717b1608e870d76
openai-exe <sabeensaeed06@gmail.com>
2023-12-08 23:13:47 +0500

    Made changes in appointments folder 5.0

--- appointments/app.py
+++ appointments/app.py
@@ -14,7 +14,7 @@ mongo = PyMongo(app)
 #   { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
 # ]
 
-@app.route('/hello9')
+@app.route('/hello90')
 def hello():
   greeting = "Hello world!"
   return greeting
