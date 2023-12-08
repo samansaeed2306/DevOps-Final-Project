82b3aadf8c89f27fa36d1c1bcad637b543b18537
openai-exe <sabeensaeed06@gmail.com>
2023-12-08 21:20:58 +0500

    Made a change inside doctors folder

--- doctors/app.py
+++ doctors/app.py
@@ -6,10 +6,15 @@ app = Flask(__name__)
 CORS(app)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/DevOps"
 mongo = PyMongo(app)
+
 # doctors = [
 #   { 'id': "1",'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality':"DevOps"  },
 #   { 'id': "2",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  }
 # ]
+@app.route('/hello2.0')
+def hello2():
+  greeting = "Hello Doctors 2.0!"
+  return greeting
 
 @app.route('/hello')
 def hello():
@@ -20,7 +25,7 @@ def hello():
 def check_mongodb_connection():
     try:
         mongo.db.doctors.find_one()
-        return jsonify({"status": "success", "message": "MongoDB connection successful"})
+        return jsonify({"status": "success", "message": "MongoDB successfully connected "})
     except Exception as e:
         return jsonify({"status": "error", "message": f"MongoDB connection error: {str(e)}"}), 500
 
